b47828746b45bc7e1dd2540bdc632bc8c5be59bc
Munir Mahmud <munirmahmud6@gmail.com>
2020-12-13 18:54:43 +0600

    Responsive & mobile menu done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,24 @@
 const bodyElement = document.querySelector('body');
 const menuToggle = bodyElement.querySelector('.hamburger-menu');
+const navLists = bodyElement.querySelector('.nav-list');
 
 menuToggle.addEventListener('click', () => {
     bodyElement.classList.toggle('is-open');
+});
+
+navLists.addEventListener('click', e => {
+    let clickedEl = e.target;
+
+    if(clickedEl.classList[0] === 'nav-link') {
+        bodyElement.classList.remove('is-open');
+    }
+});
+
+//Add Eventlistener to the window object to close navigation
+window.addEventListener('click', e => {
+    let clickedEl = e.target;
+
+    if(clickedEl.matches('.main')) {
+        bodyElement.classList.remove('is-open');
+    }
 });
